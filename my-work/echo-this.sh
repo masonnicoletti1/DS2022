dcf31810372e81c52af8ab621fac74eb7a58772a
masonnicoletti <mason.f.nicoletti@gmail.com>
2024-09-19 01:22:16 -0400

    Remove in-class test files.

--- my-work/echo-this.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VAR1="Coffee"
-
-echo "Hey $LOGNAME. Your favorite flavor of ice cream is $VAR1."
-
-
-
--- my-work/read-interactive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Input file name: " INPUT
-read -p "Output file name: " OUTPUT
-
-echo "Use $INPUT file to produce $OUTPUT."
-
-
