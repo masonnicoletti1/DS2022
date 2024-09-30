4b1eeced827137b16679c92372a6aa531f9e1509
Mason Nicoletti <mason.f.nicoletti@gmail.com>
2024-09-30 22:36:28 +0000

    Update lab4 script.

--- my-work/lab4/boto3-image.py
+++ my-work/lab4/boto3-image.py
@@ -5,6 +5,7 @@ import boto3
 import urllib.request
 
 image_url = "https://t3.ftcdn.net/jpg/06/26/71/36/360_F_626713659_a9FhZtViYUooPSvqme2ZZIIvr7Qg5yBY.jpg"
+#image_url2 = "https://c4.wallpaperflare.com/wallpaper/179/223/759/underwater-water-sun-rays-nature-wallpaper-preview.jpg"
 image_name = "underwater.jpg"
 bucket = 'ds2022-cxx6sw'
 
@@ -15,12 +16,12 @@ image = urllib.request.urlretrieve(image_url, image_name)
 s3 = boto3.client('s3', region_name='us-east-1')
 
 #Upload file to bucket
-resp = s3.put_object(
-    Body = image_name, 
-    Bucket = bucket,
-    Key = image_name,
-    ACL = 'public-read'
-)
+with open(image_name, 'wb') as file_data:
+    resp = s3.put_object(
+        Body = image_name, 
+        Bucket = bucket,
+        Key = image_name,
+    )
 
 #Presign file with expiration time
 response = s3.generate_presigned_url(
